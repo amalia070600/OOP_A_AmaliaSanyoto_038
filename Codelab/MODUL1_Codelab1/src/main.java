ef1d7677fd6bdd0d403126215876129107c780f2
amalia070600 <amaliasyt@gmail.com>
2025-02-27 02:38:24 +0700

    Update main.java

--- Codelab/MODUL1_Codelab1/src/main.java
+++ Codelab/MODUL1_Codelab1/src/main.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
-import java.time.LocalDate; //this import the LocalDate allow us to retrieve the current year to age calculation
+import java.time.LocalDate; //imports the LocalDate, which is used to retrieve the current year for age calculation.
 
 public class Main {
-    public static void main(String[] args) {
-    //to determine the gender output given by the user
+    public static void main(String[] args) { //entry point of the program
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: "); //displays a prompt asking the user to enter their name
+        String name = scanner.nextLine(); //reads the entire line entered by the user and stores it in the variable name
+
+        System.out.print("Enter your gender (M/F): ");
+        char genderChar = scanner.next().charAt(0); //reads the word input by user, and also extract the first charter from the user input
+        String gender;
+
+        // this code block, checks the user input and assigns the appropriate value to the gender variable
         if (genderChar == 'M' || genderChar == 'm') {
             gender = "Male";
         } else if (genderChar == 'F' || genderChar == 'f') {
@@ -13,16 +22,15 @@ public class Main {
         }
 
         System.out.print("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt(); //read integer input
+        int yearOfBirth = scanner.nextInt(); //read an integer input from user
+
+        int currentYear = LocalDate.now().getYear(); // retrieves the current year
+        int age = currentYear - yearOfBirth; //calculates a user's age by subtracting their year of birth from the current year
 
 
-        int currentYear = LocalDate.now().getYear(); //retrieves the current year from the system using LocalDate.
-        int age = currentYear - yearOfBirth; //calculates a user's age by subtracting their year of birth from the current year.
+        scanner.close(); //closing the scanner, to avoid potential resource leaks
 
-
-        scanner.close(); //close the scanner to avoid potential resource leaks
-
-        //display to user
+        // display user details
         System.out.println("\nUser Details:");
         System.out.println("Name: " + name);
         System.out.println("Gender: " + gender);
